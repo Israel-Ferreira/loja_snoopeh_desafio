cf506215a3d38dc3400b8ef03cb587a6594381e8
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-07-18 17:01:56 -0300

    Criando as models de carrinho e items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-
+  before_action :set_cart
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
@@ -16,6 +15,15 @@ class ApplicationController < ActionController::API
     else
       error_json = { status: '400', title: 'Bad Request', detail: resource.errors }
       render json: { errors: error_json }, status: :bad_request
+    end
+  end
+
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    if user_signed_in?
+      cart_params = { user: @current_user }
+      @cart = Cart.create(cart_params)
     end
   end
 end
