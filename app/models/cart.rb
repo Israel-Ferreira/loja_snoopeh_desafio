a375e6c61aa355f0e607434d2871912c438d7452
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-07-19 20:19:29 -0300

    cart total_value created

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items
+
+  def total_to_pay
+    cart_items.sum(&:total_price)
+  end
 end
--- app/serializers/cart_serializer.rb
+++ app/serializers/cart_serializer.rb
@@ -1,4 +1,8 @@
 class CartSerializer < ActiveModel::Serializer
-  attributes :id, :active
+  attributes :id, :active, :total_value
   has_many :cart_items
+
+  def total_value 
+    object.total_to_pay
+  end
 end
